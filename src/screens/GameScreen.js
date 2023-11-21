8a246aef7e3f1c04cf04449425ce52f3ed000841
Ivan Yeung <91327331+shu0202@users.noreply.github.com>
2023-11-21 23:11:44 +0000

    added save state of plants

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { View, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import TouchableScale from "react-native-touchable-scale";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import Header from '../components/HeaderComponent';
 import { backgrounds } from '../states/backgroundsConfig';
@@ -20,6 +21,34 @@ const GameScreen = () => {
     const [currentBackground, setCurrentBackground] = useState(backgrounds.background4);
     const backgroundImage = currentBackground.image;
     const [menuVisible, setMenuVisible] = useState(false);
+
+    useEffect(() => {
+      const loadCurrentBackground = async () => {
+        try {
+          const savedBackground = await AsyncStorage.getItem('currentBackground');
+          if (savedBackground) {
+            setCurrentBackground(JSON.parse(savedBackground));
+          }
+        } catch (error) {
+          console.log('Failed to load the current background:', error);
+        }
+      };
+  
+      loadCurrentBackground();
+    }, []);
+
+    useEffect(() => {
+      const saveCurrentBackground = async () => {
+        try {
+          await AsyncStorage.setItem('currentBackground', JSON.stringify(currentBackground));
+          console.log('Current background saved:', currentBackground);
+        } catch (error) {
+          console.log('Failed to save the current background:', error);
+        }
+      };
+    
+      saveCurrentBackground();
+    }, [currentBackground]);
 
     const handleToggleMenu = () => {
         setMenuVisible(toggleMenu(menuVisible));
