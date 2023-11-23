ae892de5657ebea4985eaf60b02438130b3b703a
Favour <dfoshidero@outlook.com>
2023-11-23 13:33:49 +0000

    style changes

--- src/styles/GameScreenStyles.js
+++ src/styles/GameScreenStyles.js
@@ -9,15 +9,15 @@ const gameStyles = StyleSheet.create({
   },
   settingsIcon: {
     position: "absolute",
-    top: "25%",
+    top: "27%",
     left: "4.5%",
     zIndex: 1,
   },
   heartsIcon: {
     zIndex: 1,
     position: "absolute",
-    top: "32%",
-    left: "4.5%",
+    top: "33%",
+    left: "4.6%",
   },
 });
 
