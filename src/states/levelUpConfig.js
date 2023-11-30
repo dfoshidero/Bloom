da49f204ae21c0777ad64ed74353468786a52a16
Favour <dfoshidero@outlook.com>
2023-11-30 17:54:54 +0000

    Updated XP needed for level up

--- src/states/levelUpConfig.js
+++ src/states/levelUpConfig.js
@@ -1,7 +1,12 @@
 const requiredXP = {
   1: { xpRequired: 0 },
-  2: { xpRequired: 1500 },
-  3: { xpRequired: 2500 },
-  3: { xpRequired: 3500 },
+  2: { xpRequired: 500 },
+  3: { xpRequired: 1250 },
+  5: { xpRequired: 2250 },
+  6: { xpRequired: 3500 },
+  7: { xpRequired: 5000 },
+  8: { xpRequired: 6750 },
+  9: { xpRequired: 8750 },
+  10: { xpRequired: 11000 },
 };
 export default requiredXP;
